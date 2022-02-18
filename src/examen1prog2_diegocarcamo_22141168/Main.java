eb00d1f4eb41b91545aa84e88b323c64fe19084e
MrMakako <diegofdiaz1717@gmail.com>
2022-02-18 12:20:24 -0600

    Primer commit

---
+++ src/examen1prog2_diegocarcamo_22141168/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen1prog2_diegocarcamo_22141168;
+
+/**
+ *
+ * @author Diego Carcamo
+ */
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        
+       //Iniciar Examen
+       
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
